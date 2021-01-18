361e6dc72fb3467cab3af5138311632dc5f443b1
Keks <keks@htmlacademy.ru>
2021-01-18 15:17:46 +0300

    :heavy_check_mark: Сборка #18

---
+++ 18/js/script.js
@@ -0,0 +1,25 @@
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-navigation__toggle');
+const buttonBefore = document.querySelector('.slider__label--before');
+const buttonAfter = document.querySelector('.slider__label--after');
+const imageBefore = document.querySelector('.example__image--before');
+const imageAfter = document.querySelector('example__image--after');
+
+navMain.classList.remove('main-navigation--no-js');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
+
+buttonAfter.onclick = function() {
+  if (imageAfter.classList.contains('example__image--noshow')) {
+    imageAfter.classList.remove('example__image--noshow');
+    imageBefore.classList.add('example__image--noshow');
+  }
+}
